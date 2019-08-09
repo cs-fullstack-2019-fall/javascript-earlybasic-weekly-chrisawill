8c99c0692f738a45d65b680e13fb87f730b084d3
Christian Williams <chrisawill05@gmail.com>
2019-08-09 15:23:29 -0500

    1st push for project1. incomplete

---
+++ index.js
@@ -0,0 +1,46 @@
+// var beginP = parseInt(prompt("Welcome to Kenn's program. Press 1 to count. Press 2 for calculater. Press 'q' to quit"));
+
+var userinput = prompt("Welcome to Kenn's program. Press 1 to count. Press 2 for calculater. Press 'q' to quit");
+ var beginP = ""
+ quit = false
+
+if (beginP === 1)
+{
+	var n = ""
+	while(userinput)
+{
+var userN = prompt("Enter a number to count.");
+n=++
+console.log(n)
+}
+}
+
+
+
+
+
+
+
+
+
+// if (userinput==="")
+// var input = prompt("Enter a number to count to.");
+// 	input = ""
+// {
+// while (input>0)
+// {
+// 	n=++
+// console.log("")
+// }
+// }
+
+// var userinput = prompt("Welcome to Kenn's program. Press '1' to count. Press '2' for calculator. Press 'q' to quit. ");
+
+// if (userinput === 1);
+// {
+//  prompt("Enter a number to start counting");
+// var n =  ""
+
+// while(n>0)
+// {
+// 	n++
