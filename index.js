54dd9fa65ddc9c44c2a6071c39aeb59eb4f3e686
Christian Williams <chrisawill05@gmail.com>
2019-08-09 16:23:24 -0500

    My second push. Same stuff with added faliures of code. i did manage to have it ask the numbers i want to count and numbers i want to add.

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-// var beginP = parseInt(prompt("Welcome to Kenn's program. Press 1 to count. Press 2 for calculater. Press 'q' to quit"));
+//FIRST ATTEMPT #2-deleted the first two outcoms because of too many rd flags. thought it was easier to start again 
 
 var userinput = prompt("Welcome to Kenn's program. Press 1 to count. Press 2 for calculater. Press 'q' to quit");
  var beginP = ""
- quit = false
 
-if (beginP === 1)
+if(userinput = 1);
 {
-	var n = ""
-	while(userinput)
+
+	var n = "";
+	n=prompt("Pick a number to count from.")
+	while(n > 0 && n >= 0)//#3-Browser keeps chrashing since i changed it from (n<=>0) to (n >= 0). could be i typed 
+		//it wrong. couldve made another infinite loop...
+//#4-assuming someone would start in positives kept it at greater than.
+	{
+		n++
+	}
+
+}
+if (userinput =2)  
 {
-var userN = prompt("Enter a number to count.");
-n=++
-console.log(n)
-}
+ var userinput3 = prompt("Please enter the first number.");
+ var userinput4 = prompt("Please enter the second number.");
 }
 
 
@@ -20,8 +27,28 @@ console.log(n)
 
 
 
+// 	var n = ""
+// 	while(userinput)
+// {
+// var userN = prompt("Enter a number to count.");
+// n=++
+// console.log("prompt")
+// }
 
 
+
+
+// alert("Enter 2 numbers to add.");	
+
+// var userinput3 = prompt("Please enter the first number.");
+// var userinput4 = prompt("Please enter the second number.");
+// var numberInt = parseInt(userinput3);
+// var numberInt2 = parseInt2(userinput4);
+
+// alert("The sum of your numbers are " + numberInt+numberInt2);
+
+
+//SECOND ATTEMPT
 
 // if (userinput==="")
 // var input = prompt("Enter a number to count to.");
