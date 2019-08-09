056e6de70fc78e74d460e4cd12ecf6d3a38dcf3a
Christian Williams <chrisawill05@gmail.com>
2019-08-09 16:55:38 -0500

    I was able to get then alculator working. but then the initial prompt and count propmts stoped working. removed and lowered the conting below to work on the begginng prompt bit by bit.

--- index.js
+++ index.js
@@ -1,39 +1,55 @@
-//FIRST ATTEMPT #2-deleted the first two outcoms because of too many rd flags. thought it was easier to start again 
+// // // //FIRST ATTEMPT #2-deleted the first two outcoms because of too many rd flags. thought it was easier to start again 
+
 
 var userinput = prompt("Welcome to Kenn's program. Press 1 to count. Press 2 for calculater. Press 'q' to quit");
- var beginP = ""
+ // var userinput = 
+ // var beginP = ""
 
 if(userinput = 1);
 {
 
 	var n = "";
-	n=prompt("Pick a number to count from.")
+	n=prompt("Pick a number to count from.");
 	while(n > 0 && n >= 0)//#3-Browser keeps chrashing since i changed it from (n<=>0) to (n >= 0). could be i typed 
 		//it wrong. couldve made another infinite loop...
 //#4-assuming someone would start in positives kept it at greater than.
-	{
-		n++
-	}
 
-}
-if (userinput =2)  
+
+
+
+
+
+alert("Enter 2 numbers to add.");	
+
+var userinput3 = prompt("Please enter the first number.");
+var userinput4 = prompt("Please enter the second number.");
+var numberInt1 = parseInt(userinput3);
+var numberInt2 = parseInt(userinput4)
+
+alert("The sum of your numbers are " + (numberInt1+numberInt2));
+
+var userInpact = ""
+var userQ = true
+while(userQ)
 {
- var userinput3 = prompt("Please enter the first number.");
- var userinput4 = prompt("Please enter the second number.");
+	userInpact = prompt("You entered " + userInpact + ". Press q to quit or enter another equation.")
+
+if (userInpact === 'q');
+{ 
+	quit = !quit
 }
 
 
+// alert("Enter 2 numbers to add.");	
 
+// var userinput3 = prompt("Please enter the first number.");
+// var userinput4 = prompt("Please enter the second number.");
+// var numberInt1 = parseInt(userinput3);
+// var numberInt2 = parseInt(userinput4)
 
-
-
-// 	var n = ""
-// 	while(userinput)
-// {
-// var userN = prompt("Enter a number to count.");
-// n=++
-// console.log("prompt")
+// alert("The sum of your numbers are " + (numberInt1+numberInt2));
 // }
+
 
 
 
@@ -42,13 +58,16 @@ if (userinput =2)
 
 // var userinput3 = prompt("Please enter the first number.");
 // var userinput4 = prompt("Please enter the second number.");
-// var numberInt = parseInt(userinput3);
-// var numberInt2 = parseInt2(userinput4);
+// var numberInt1 = parseInt(userinput3);
+// var numberInt2 = parseInt(userinput4)
 
-// alert("The sum of your numbers are " + numberInt+numberInt2);
+// alert("The sum of your numbers are " + (numberInt1+numberInt2));
 
 
-//SECOND ATTEMPT
+
+
+
+// //SECOND ATTEMPT
 
 // if (userinput==="")
 // var input = prompt("Enter a number to count to.");
@@ -70,4 +89,4 @@ if (userinput =2)
 
 // while(n>0)
 // {
-// 	n++
+//
