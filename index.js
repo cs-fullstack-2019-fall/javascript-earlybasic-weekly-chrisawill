095ec5eb2e11188d17504223539bffa58fe23f55
Kenn <kenn+git@code-crew.org>
2019-08-12 14:20:36 -0500

    kenn updates

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ var userinput = prompt("Welcome to Kenn's program. Press 1 to count. Press 2 for
  // var userinput = 
  // var beginP = ""
 
+// There should be an overall while loop to ask over and over again.
+
+// This if statement doesn't have a closing curly brace.
 if(userinput = 1);
 {
 
 	var n = "";
 	n=prompt("Pick a number to count from.");
+	
+	// This while loop is incomplete. It doesn't have curly braces and code to execute. You'll need to change a variable in the conditional statement to make sure it can end correctly.
 	while(n > 0 && n >= 0)//#3-Browser keeps chrashing since i changed it from (n<=>0) to (n >= 0). could be i typed 
 		//it wrong. couldve made another infinite loop...
 //#4-assuming someone would start in positives kept it at greater than.
@@ -18,7 +23,7 @@ if(userinput = 1);
 
 
 
-
+// This should be in the calculator area and sectioned off by an if statement.
 alert("Enter 2 numbers to add.");	
 
 var userinput3 = prompt("Please enter the first number.");
@@ -30,6 +35,8 @@ alert("The sum of your numbers are " + (numberInt1+numberInt2));
 
 var userInpact = ""
 var userQ = true
+
+// This while loop doesn't have an ending curly brace.
 while(userQ)
 {
 	userInpact = prompt("You entered " + userInpact + ". Press q to quit or enter another equation.")
@@ -40,6 +47,7 @@ if (userInpact === 'q');
 }
 
 
+// This is a copy of the sum code from above
 // alert("Enter 2 numbers to add.");	
 
 // var userinput3 = prompt("Please enter the first number.");
@@ -53,7 +61,7 @@ if (userInpact === 'q');
 
 
 
-
+// This is a copy of the sum code from above
 // alert("Enter 2 numbers to add.");	
 
 // var userinput3 = prompt("Please enter the first number.");
